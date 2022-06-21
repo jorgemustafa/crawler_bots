8ec66e57a9e0f90bf7d05c40a61cc3f492129179
jorgemustafa <jorge.mustafa@itmss.com.br>
2022-06-21 16:03:27 -0300

    Making project public

--- engine/settings.py
+++ engine/settings.py
@@ -8,10 +8,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9uyhzbd!5wuve*0l=nowb9br9*9_1&#drb-5sk+l*_*y)(!f#!'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG')
 
 ALLOWED_HOSTS = []
 
